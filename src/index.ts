ba2d30d73038ec2f540bfe8002475a8f4191a93f
Vudang <danglegiavu@gmail.com>
2021-03-02 23:09:07 +0700

    setup express + grapql + apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,34 @@
 import {MikroORM} from "@mikro-orm/core"
 import { ___DB_NAME___, ___DB_PASSWORD___, ___DB_TYPE___, ___DB_USER___, ___PROD___ } from "./constants"
 
-import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
+
+import express from 'express';
+
+import {buildSchema} from 'type-graphql'
+
+import {ApolloServer} from 'apollo-server-express'
+import { HelloResolver } from "./resolvers/hello";
 
 const root = async () => {
   const orm = await MikroORM.init(mikroOrmConfig)
 
   await orm.getMigrator().up();
 
-  const post = await orm.em.find(Post,{});
-  console.log(post);
+  const app =express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers:[HelloResolver],
+      validate:false,
+    })
+  })
+
+  apolloServer.applyMiddleware({app});
+
+  app.listen(4000,() => {
+    console.log("Server started successfully");
+  })
   
 }
 
